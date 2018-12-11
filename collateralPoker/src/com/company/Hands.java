fe9fc4fa7543da4b4d9d7bf2bd546af3995b7075
Grzegorz Derba <grzegorzderba@protonmail.com>
2018-12-11 01:17:34 +0100

    Change enum names to 2-signs for hand

--- collateralPoker/src/com/company/Hands.java
+++ collateralPoker/src/com/company/Hands.java
@@ -2,13 +2,13 @@ package com.company;
 
 public enum Hands {
     HC_9(0), HC_T(1), HC_J(2), HC_Q(3), HC_K(4), HC_A(5),
-    P_9(6), P_T(7), P_J(8), P_Q(9), P_K(9), P_A(10),
+    PA_9(6), PA_T(7), PA_J(8), PA_Q(9), PA_K(9), PA_A(10),
     TP_T9(11), TP_J9(12), TP_JT(13), TP_Q9(14), TP_QT(15),
     TP_QJ(16), TP_K9(17), TP_KT(18), TP_KJ(19), TP_KQ(19),
     TP_A9(20), TP_AT(21), TP_AJ(22), TP_AQ(23), TP_AK(24),
     TK_9(25), TK_T(26), TK_J(27), TK_Q(28), TK_K(29), TK_A(30),
-    S_SMALL(31), S_BIG(32),
-    F_DIAMONDS(31), F_HEARTS(31), F_SPADE(31), F_CLUBS(31),
+    ST_SMALL(31), ST_BIG(32),
+    FL_DIAMONDS(31), FL_HEARTS(31), FL_SPADE(31), FL_CLUBS(31),
     FH_9T(32), FH_9J(33), FH_9Q(34), FH_9K(35), FH_9A(36),
     FH_T9(37), FH_TJ(38), FH_TQ(39), FH_TK(40), FH_TA(41),
     FH_J9(42), FH_JT(43), FH_JQ(44), FH_JK(45), FH_JA(46),
